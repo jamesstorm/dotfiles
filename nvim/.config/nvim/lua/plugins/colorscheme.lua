e09c299cc0dc87bc9b4e8a14d507ec0fb16b7ca9
James Storm <jamesstorm@gmail.com>
2024-09-20 13:12:34 -0400

    let's try this Catppuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
-return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-}
+--return {
+--  {
+--    "folke/tokyonight.nvim",
+--    opts = {
+--      transparent = true,
+--      styles = {
+--        sidebars = "transparent",
+--        floats = "transparent",
+--      },
+--    },
+--  },
+--}
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }

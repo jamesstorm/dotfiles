98df26a2396bff574ccbc58d9d18d7b85465f48a
James Storm <jamesstorm@gmail.com>
2024-09-22 10:57:33 -0400

    harpoon keys for me

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,7 +12,7 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function()
   harpoon:list():add()
 end)
-vim.keymap.set("n", "<C-l>", function()
+vim.keymap.set("n", "<C-y>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 

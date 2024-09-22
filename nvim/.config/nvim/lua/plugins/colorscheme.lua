6fc099967d82f98ea766c7f370f18376dd84fd1c
James Storm <jamesstorm@gmail.com>
2024-09-22 10:57:09 -0400

    catppuccin colorscheme with line at 80 columns

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
---return {
---  {
---    "folke/tokyonight.nvim",
---    opts = {
---      transparent = true,
---      styles = {
---        sidebars = "transparent",
---        floats = "transparent",
---      },
---    },
---  },
---}
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  { "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+      overrrides = {
+        vim.api.nvim_set_option_value("colorcolumn", "80", {}),
+      },
+    },
+  },
+}

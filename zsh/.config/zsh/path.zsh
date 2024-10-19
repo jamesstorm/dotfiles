c3e9fa113e5528cd030d6b7fe9034b40bed4a810
James Storm <jamesstorm@gmail.com>
2024-10-19 19:56:54 -0400

    ketchup

--- zsh/.config/zsh/path.zsh
+++ zsh/.config/zsh/path.zsh
@@ -1,2 +1,2 @@
 
-export PATH=$PATH:/Users/james/.local/bin:/home/james/.local/bin:/home/james/.local/bin/nvim-linux64/bin:/Users/james/.local/bin/nvim-macos-x86_64/bin:/opt/nvim-linux64/bin:/usr/bin:/usr/bin/core_perl:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/lib/jvm/default/bin:/usr/local/bin:/usr/local/sbin:/var/lib/flatpak/exports/bin
+export PATH=$PATH:/home/james/.cargo/bin:/Users/james/.local/bin:/home/james/.local/bin:/home/james/.local/bin/nvim-linux64/bin:/Users/james/.local/bin/nvim-macos-x86_64/bin:/opt/nvim-linux64/bin:/usr/bin:/usr/bin/core_perl:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/lib/jvm/default/bin:/usr/local/bin:/usr/local/sbin:/var/lib/flatpak/exports/bin
